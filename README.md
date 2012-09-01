f4a5ead7f0e273958d44c904aae3033cd04dd5c1
Greg Guthe <gregg@aweber.com>
2012-09-01 15:55:32 -0400

    Hide playlist comments too

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 A Chrome extension to hide soundcloud comments.
 
-Literally one line of jQuery from:
 From: http://webapps.stackexchange.com/questions/20868/hide-comments-by-default-on-soundcloud
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.info('Using hide soundcloud comments extension content script');
 
-// From: http://webapps.stackexchange.com/questions/20868/hide-comments-by-default-on-soundcloud
-
 // Assuming the player loads after the page
-$(document).on('DOMNodeInserted', '.player', function() {
+$(document).on('DOMNodeInserted', '.player, .playlist-item', function() {
   $(this).addClass('no-comments');
 });
